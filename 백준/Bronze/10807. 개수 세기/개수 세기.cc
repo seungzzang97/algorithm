5372e651dfb9a98729fd101b539e625c0412e148
SEUNGHO PARK <77253382+seungzzang97@users.noreply.github.com>
2024-01-17 23:55:21 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.cc
+++ 백준/Bronze/10807. 개수 세기/개수 세기.cc
@@ -1,36 +1,17 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+#include <stdio.h>
+#include <string.h>
 
-int main() {
 
-	int list[100], n, s, count=0;
-	cin >> n;
-
+int cnt[201], w = 100;
+int main()
+{
+	int n, v;
+	scanf("%d", &n);
 	for (int i = 0; i < n; i++) {
-		cin >> list[i];
+		scanf("%d", &v);
+		cnt[v + w]++;
 	}
-	
-	cin >> s;
+	scanf("%d", &v);
+	printf("%d\n", cnt[v + w]);
 
-	for (int i = 0; i < n; i++) {
-		if (list[i] == s) {
-			count++;
-		}
-	}
-
-	cout << count;
-	
 }
-
-/*
-문제
-총 N개의 정수가 주어졌을 때, 정수 v가 몇 개인지 구하는 프로그램을 작성하시오.
-
-입력
-첫째 줄에 정수의 개수 N(1 ≤ N ≤ 100)이 주어진다.둘째 줄에는 정수가 공백으로 구분되어져있다.
-셋째 줄에는 찾으려고 하는 정수 v가 주어진다.입력으로 주어지는 정수와 v는 - 100보다 크거나 같으며, 100보다 작거나 같다.
-
-출력
-첫째 줄에 입력으로 주어진 N개의 정수 중에 v가 몇 개인지 출력한다.
-*/
